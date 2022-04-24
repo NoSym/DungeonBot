ce174934d820885cba0fd48768c2b4938da71b7c
NoSym <50124662+NoSym@users.noreply.github.com>
2022-04-24 09:28:37 -0400

    More project adjustments

--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 - Create a .env file in top level directory and declare the following environment variables
 
 ```
+ADMIN_USER_ID
 DISCORD_CLIENT_ID
-DISCORD_GUILD_ID
 DISCORD_TOKEN
+TEST_GUILD_ID
 ```
---
+++ scripts/deploy-commands.js
@@ -0,0 +1,28 @@
+const fs = require('node:fs')
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const dotenv = require('dotenv')
+
+dotenv.config();
+
+const commands = []
+const commandFiles = fs.readdirSync(`${__dirname}/../src/commands`).filter((file) => file.endsWith('.ts'))
+const environment = process.argv.length > 0 ? process.argv[0] : 'test'
+
+for (const file of commandFiles) {
+    const command = require(`${__dirname}/../src/commands/${file}`).default
+    
+    commands.push(command.data.toJSON())
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN)
+
+if (environment === 'test') {
+    rest.put(Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, process.env.TEST_GUILD_ID), { body: commands })
+    .then(() => console.log('Successfully registered guild application commands.'))
+    .catch(console.error)
+} else if (environment === 'prod') {
+    rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID, process.env.TEST_GUILD_ID), { body: commands })
+    .then(() => console.log('Successfully registered global application commands.'))
+    .catch(console.error)
+}
