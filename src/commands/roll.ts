07e32a770d041ef0b9b3166d0cbae256bb44397c
NoSym <50124662+NoSym@users.noreply.github.com>
2022-05-01 15:47:02 -0400

    Added log output files, minor changes

--- src/commands/roll.ts
+++ src/commands/roll.ts
@@ -4,11 +4,13 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 
 const NUMBER_DICE = 'dice'
 const NUMBER_SIDES = 'sides'
+const BONUS = 'bonus'
 const ROLL_LABEL = 'label'
 
 const execute = async (interaction: CommandInteraction) => {
     const numberOfDice = interaction.options.getNumber(NUMBER_DICE, true)
     const numberOfSides = interaction.options.getNumber(NUMBER_SIDES, true)
+    const bonusModifier = interaction.options.getNumber(BONUS)
     const label = interaction.options.getString(ROLL_LABEL)
     const labelText = label ? ` for ${label}` : ``
     const results: number[] = []
@@ -17,11 +19,13 @@ const execute = async (interaction: CommandInteraction) => {
         results.push(Math.floor(Math.random() * (numberOfSides + 1)) + 1)
     }
 
-    const sum = results.reduce((x1, x2) => x1 + x2)
+    const sum = results.reduce((x1, x2) => x1 + x2) + (bonusModifier ?? 0)
+    const bonusTitleText = bonusModifier ? ` + ${bonusModifier} ` : ''
+    const bonusDescText = bonusModifier ? ` (${sum} + ${bonusModifier})` : ''
     
     const embeddedResponse = new MessageEmbed()
-        .setTitle(`Rolling ${numberOfDice}d${numberOfSides}${labelText}`)
-        .setDescription(`${sum}`)
+        .setTitle(`Rolling ${numberOfDice}d${numberOfSides}${bonusTitleText}${labelText}`)
+        .setDescription(`${sum}${bonusDescText}`)
 
     if (numberOfDice > 1) {
         embeddedResponse.addField('Rolls', results.join(', '), true)
@@ -46,6 +50,11 @@ const roll: CustomCommand = {
                 .setMinValue(1)
                 .setMaxValue(1000000)
                 .setRequired(true))
+        .addNumberOption(option =>
+            option.setName(BONUS)
+                .setDescription('Bonus modifier')
+                .setMinValue(1)
+                .setMaxValue(1000000))
         .addStringOption(option => 
             option.setName(ROLL_LABEL)
                 .setDescription('What are you rolling for?')),
