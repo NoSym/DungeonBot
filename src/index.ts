cc98deca726f80d11408791d1a9f1c7eeeb1fee5
NoSym <50124662+NoSym@users.noreply.github.com>
2022-10-12 22:03:36 -0400

    Added test env discord token

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,14 @@ import { Intents } from 'discord.js'
 import { CustomClient } from './classes/CustomClient'
 import 'dotenv/config'
 
+const ENV_TEST = 'test'
+const ENV_PROD = 'prod'
+
+const environment = process.argv.length > 2 ? process.argv[2] : ENV_TEST
+const token = environment === ENV_PROD ? process.env.DISCORD_TOKEN : process.env.DISCORD_TOKEN_TEST
+
 // Create a new client instance
 const client = new CustomClient({ intents: [Intents.FLAGS.GUILDS] })
 
 // Login to Discord with your client's token
-client.login(process.env.DISCORD_TOKEN)
+client.login(token)
